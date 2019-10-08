b627c17cabd06ac78d003733edf9266781cdfa04
Nadiaabr <nadia@abramian.net>
2019-10-08 11:23:03 -0700

    triggered button
    The form should now appear on the page when a call button is triggered

--- HW_Class6/main.js
+++ HW_Class6/main.js
@@ -13,7 +13,6 @@ function myFunction() {
 }
 
 
-
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
   'use strict';
@@ -33,36 +32,16 @@ function myFunction() {
   }, false);
 })();
 
+$(document).ready(function(){
+  $("#checkoutBotton").click(function(){
+    $("#checkoutForm").toggle();
+  });
+  $("#show").click(function(){
+    $("p").show();
+  });
+});
 
 
 
 
 
-
-$(document).ready(function () {
-  //$("#mainTitle").addClass("pink");
-
-  $("#colorChange").click(changeColor);
-  $("#addDiv").click(addStuff);
-
-  function changeColor() {
-    $("#mainTitle").toggleClass("blue");
-    $("#subTitle").toggle();
-    // if( $("#mainTitle").hasClass("pink") ){
-    //     $("#mainTitle").removeClass("pink");
-    //     $("#mainTitle").addClass("blue");
-    // }
-    // else if( $("#mainTitle").hasClass("blue") ) {
-    //     $("#mainTitle").removeClass("blue");
-    //     $("#mainTitle").addClass("pink");
-    // }
-    // else {
-    //     $("#mainTitle").addClass("pink");
-    // }
-  }
-
-  function addStuff() {
-    $("#mainTitle").append('<span style="color: red">Ha, ha, ha!</span>');
-  }
-
-})
