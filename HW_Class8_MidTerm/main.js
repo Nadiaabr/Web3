458a71fc066e4b83d44df47df86d3e04abc9fd76
Nadiaabr <nadia@abramian.net>
2019-10-22 05:54:19 -0700

    carusel

--- HW_Class8_MidTerm/main.js
+++ HW_Class8_MidTerm/main.js
@@ -2,13 +2,5 @@
 
 
 
-$('.carousel-control-prev').click(function(e){
-  e.stopPropagation();
-  var goTo = $(this).data('slide');
-  if(goTo=="prev") {
-      $('#carousel-id').carousel('prev'); 
-  } else {
-      $('#carousel-id').carousel('next'); 
-  }
-});
+
 
