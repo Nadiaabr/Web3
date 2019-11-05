53c09523d9836553960c92ada8bb83b15eee66cc
Nadiaabr <nadia@abramian.net>
2019-11-04 22:37:44 -0800

    animated img of carusel
    carusel animation
    from {filter: grayscale(100%);}
       to {filter: grayscale(0%);}

--- HW_Class10/main.js
+++ HW_Class10/main.js
@@ -1,1 +1,9 @@
-$('.carousel').carousel()
+$('.carousel').carousel({
+  interval: 10000
+})
+
+$('.carousel').on('slide.bs.carousel', function onSlide (ev) {
+  var img = ev.relatedTarget.querySelector("img");
+  img.classList.remove("grayscale-to-color");
+  img.classList.add("grayscale-to-color");
+})
