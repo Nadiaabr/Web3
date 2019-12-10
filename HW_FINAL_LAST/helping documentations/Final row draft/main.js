51afdb6f1a110c335ea32eb02ecc4fb211017db8
Nadiaabr <nadia@abramian.net>
2019-12-10 13:51:05 -0800

    final

--- HW_FINAL_LAST/helping documentations/Final row draft/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-
-  $('.animated-button').on('click', function () {
-
-    $('.animated-icon').toggleClass('open');
-  });
-});
--- HW_FINAL_LAST/main.js
+++ HW_FINAL_LAST/main.js
@@ -1,3 +1,20 @@
+function myFunction() {
+  // Get the checkbox
+  var checkBox = document.getElementById("myCheck");
+  // Get the output text
+  var text = document.getElementById("text");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true) {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+
+
+
 function updateHeaderLogo() {
   var logoWidth = 150; // in pixels
   var marginX = logoWidth / 2 + 15; // +margin
@@ -115,6 +132,8 @@ $(document).ready(function() {
     return false;
   });
 });
+
+
 
 
 
